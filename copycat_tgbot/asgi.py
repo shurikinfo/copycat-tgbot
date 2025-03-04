0df8061cbd4ca2b7a405de5e66b19d0d53393d1a
--global <shurik@shurik.info>
2025-03-04 20:43:16 +0300

    Запускать uvicorn через entrypoint внутри контейнера

--- copycat_tgbot/asgi.py
+++ copycat_tgbot/asgi.py
@@ -1,14 +1,9 @@
 import logging
 
-import uvicorn
-
 from copycat_tgbot.app import App
-from copycat_tgbot.constants import UVICORN_LOG_CONFIG
 
 logger = logging.getLogger(__name__)
 
 app = App()
 app.init_app()
 app.init_server()
-
-uvicorn.run(app.server.app, host="localhost", port=8000, log_config=UVICORN_LOG_CONFIG)
